b0f939cca9dc99e3664be39f7cc74f941677718f
keisuke <n.kei.0801.1997@gmail.com>
2022-03-20 22:21:51 +0900

    mypage

--- resources/views/user/mypage.blade.php
+++ resources/views/user/mypage.blade.php
@@ -10,19 +10,23 @@
         会員番号：{{$user_id}}
       </div>
       <div>
-        <a class="btn" href="/mypage/offer_content/{{$user_id}}">募集している内容を確認する</a>
+        <a class="btn" href="/mypage/reserve_round">今後のラウンド予定</a>
       </div>
 
       <div>
-        <a class="btn" href="/mypage/reserve_content/{{$user_id}}">予約内容を確認する</a>
+        <a class="btn" href="/mypage/creditcard">クレジットカード情報</a>
       </div>
 
       <div>
-        <a class="btn" href="/chat_select">チャット</a>
+        <a class="btn" href="/mypage/receipt">ラウンド履歴・領収書発行</a>
       </div>
 
       <div>
         <a class="btn" href="/mypage/profile/{{$user_id}}">登録情報内容を確認する</a>
+      </div>
+
+      <div>
+        <a href="/mypage/driver_license" class="btn">運転免許証の提出</a>
       </div>
 
       <div>
