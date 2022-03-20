badcbecc1445d1d2515ab645ced2101571fa16ef
keisuke <n.kei.0801.1997@gmail.com>
2022-03-20 15:16:16 +0900

    linelogin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,11 @@
 <body>
     <div>
 
-Hello
+        @auth
+        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+        @endauth
+
+        <a href="{{route('linelogin')}}">LINEログイ</a>
 
     </div>
 
