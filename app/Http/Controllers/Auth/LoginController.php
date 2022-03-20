d855e082f2460eecd11da9989b9f44f8be82857d
keisuke <n.kei.0801.1997@gmail.com>
2022-03-20 16:01:54 +0900

    mypage

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -74,7 +74,7 @@ class LoginController extends Controller
         Auth::login($user);
         $user_id = Auth::id();
 
-        return redirect()->route('getMypage')->with(['user_id' => $user_id]);
+        return redirect()->route('getMypage',['user_id' => $user_id]);
     }
 
     /**
