e8a6f3c1e09db31f42cca2bc7edf599def8b969b
keisuke <n.kei.0801.1997@gmail.com>
2022-03-21 00:36:41 +0900

    chat

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -8,6 +8,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
   <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
 </head>
