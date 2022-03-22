3ed602f8ca33e6cecc0aca8dd21a706e2aee3a0c
keisuke <n.kei.0801.1997@gmail.com>
2022-03-23 02:02:22 +0900

    auth

--- app/Http/Controllers/Companion/Auth/CompanionLoginController.php
+++ app/Http/Controllers/Companion/Auth/CompanionLoginController.php
@@ -10,7 +10,8 @@ use Illuminate\Support\Facades\Auth; //追記
 use Laravel\Socialite\Facades\Socialite; //追記
 use App\Models\User; //追記
 use App\Models\Companion; //追記
-use Illuminate\Http\Request;//追記
+use Illuminate\Http\Request; //追記
+use Illuminate\Support\Str;//追記
 
 
 class CompanionLoginController extends Controller
@@ -47,35 +48,102 @@ class CompanionLoginController extends Controller
 
     public function cast_redirectToProvider()
     {
-        return Socialite::driver('line')->redirect(route('companion.callback'));
+        $state = Str::random(32);
+        $nonce  = Str::random(32);
+
+        $uri = "https://access.line.me/oauth2/v2.1/authorize?";
+        $response_type = "response_type=code";
+        $client_id = "&client_id=" . config('services.line.client_id');
+        $redirect_uri = "&redirect_uri=" . config('services.line.redirect2');
+        $state_uri = "&state=" . $state;
+        $scope = "&scope=openid%20profile";
+        $prompt = "&prompt=consent";
+        $nonce_uri = "&nonce=";
+
+        $uri = $uri . $response_type . $client_id . $redirect_uri . $state_uri . $scope . $prompt . $nonce_uri;
+
+        return redirect($uri);
     }
 
+
+    // アクセストークン取得
+    public function getAccessToken($req)
+    {
+
+        $headers = ['Content-Type: application/x-www-form-urlencoded'];
+        $post_data = array(
+            'grant_type'    => 'authorization_code',
+            'code'          => $req['code'],
+            'redirect_uri'  => config('services.line.redirect2'),
+            'client_id'     =>  config('services.line.client_id'),
+            'client_secret' => config('services.line.client_secret'),
+        );
+        $url = 'https://api.line.me/oauth2/v2.1/token';
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post_data));
+
+        $res = curl_exec($curl);
+        curl_close($curl);
+        $json = json_decode($res);
+        $accessToken = $json->access_token;
+
+        return $accessToken;
+    }
+
+    // プロフィール取得
+    public function getProfile($at)
+    {
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $at));
+        curl_setopt($curl, CURLOPT_URL, 'https://api.line.me/v2/profile');
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $res = curl_exec($curl);
+        curl_close($curl);
+
+        $json = json_decode($res);
+
+        return $json;
+    }
     /**
      * Obtain the user information from the provider.
      *
      * @param string $provider
      * @return \Illuminate\Http\Response
      */
-    public function handleProviderCallback(Request $request)
+    public function cast_handleProviderCallback(Request $request)
     {
-        $provided_user = Socialite::driver('line')->user();
+        $accessToken = $this->getAccessToken($request);
+        $profile = $this->getProfile($accessToken);
 
-        $user = Companion::where('line_id', $provided_user->id)
-            ->first();
+        // ユーザー情報あるか確認
+        $user = Companion::where('line_id', $profile->userId)->first();
 
-        if ($user === null) {
-            // redirect confirm
-            $user = Companion::create([
-                'name'               => $provided_user->name,
-                'provider'           => 'line',
-                'line_id'   => $provided_user->id,
-            ]);
+        // あったらログイン
+        if ($user) {
+            Auth::login($user);
+            $user_id = Auth::id();
+            return redirect('getMypage',['user_id'=>$user_id]);
+
+            // なければ登録してからログイン
+        } else {
+            $user = new Companion();
+            $user->provider = 'line';
+            $user->line_id = $profile->userId;
+            $user->name = $profile->displayName;
+            $user->save();
+            Auth::login($user);
+            $user_id = Auth::id();
+            return redirect('getMypage',[$user_id]);
         }
-
-        Auth::login($user);
-        $user_id = Auth::id();
-
-        return redirect()->route('getMypage',['user_id' => $user_id]);
     }
 
     /**
