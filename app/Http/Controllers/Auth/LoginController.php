187a4c46b9c68c047d1f6504aeab456689ecedf8
keisuke <n.kei.0801.1997@gmail.com>
2022-03-23 01:33:37 +0900

    auth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -46,7 +46,7 @@ class LoginController extends Controller
 
     public function redirectToProvider()
     {
-        return Socialite::driver('line')->redirect(route('callbacking'));
+        return Socialite::driver('line')->redirect(route('callback'));
     }
 
     /**
