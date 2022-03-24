c4e206dfcc34bab9b01a9066d2cfdf87f3ce19d1
keisuke <n.kei.0801.1997@gmail.com>
2022-03-24 23:59:42 +0900

    accept

--- app/Http/Controllers/ReserveController.php
+++ app/Http/Controllers/ReserveController.php
@@ -4,16 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Models\Reserve;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Offer;
+use App\Models\Area;
+use App\Models\Level;
+use App\Models\User;
 
 class ReserveController extends Controller
 {
     public function AcceptDone(Request $request){
 
-        $param =[
-            'offer_id' => $request->offer_id
-        ];
+        $offer_id = $request['offer_id'];
 
-        Reserve::create($param);
+        Reserve::create($offer_id);
+
+        $companion_id = Auth::id();
+        $invites = Offer::where('companion_id', $companion_id)->get();
+        foreach ($invites as $invite) {
+
+            $user = User::where('id', $invite->user_id)->first();
+            $invite->user_name = $user->nickname;
+            $invite->image = $user->img_url;
+
+            $area = Area::where('id', $invite->area_id)->first();
+            $invite->area_name = $area->name;
+        }
 
         return redirect()->route('CastGetInvited');
     }
