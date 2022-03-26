9345de21f65863adfd1d4b228a69dd9c920efea9
keisuke <n.kei.0801.1997@gmail.com>
2022-03-26 12:11:12 +0900

    reserve

--- app/Models/Reserve.php
+++ app/Models/Reserve.php
@@ -11,15 +11,6 @@ class Reserve extends Model
     protected $fillable = [
         'user_id',
         'companion_id',
-        'area_id',
-        'golf_course',
-        'date',
-        'start_at',
-        'num_of_players_men',
-        'num_of_players_women',
-        'mens_level_id',
-        'required_level_id',
-        'required_age_min',
-        'required_age_max',
+        'offer_id'
     ];
 }
