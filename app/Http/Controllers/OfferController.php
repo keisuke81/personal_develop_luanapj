fcaf82e6a3f82bd393774a0027b1575ebb9afc48
keisuke <n.kei.0801.1997@gmail.com>
2022-03-26 13:07:19 +0900

    reserve

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -60,7 +60,7 @@ class OfferController extends Controller
         $companion_id = $id;
 
         if ($request->get('back')) {
-            return redirect()->route('InviteConfirm')->withInput();
+            return redirect()->route('InviteConfirm',['id'=> $id])->withInput();
         }else{
 
             $param = [
