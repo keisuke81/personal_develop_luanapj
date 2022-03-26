072db22f77ca868b6d6b43ec51242b0e59b9d43d
keisuke <n.kei.0801.1997@gmail.com>
2022-03-26 12:32:57 +0900

    reserve

--- resources/views/user/mypage.blade.php
+++ resources/views/user/mypage.blade.php
@@ -10,7 +10,7 @@
         会員番号：{{$user_id}}
       </div>
       <div>
-        <a class="btn" href="/mypage/user_reserve_round">今後のラウンド予定</a>
+        <a class="btn" href="{{route('GetUserReserve')}}">今後のラウンド予定</a>
       </div>
 
       <div>
