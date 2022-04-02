794dad6c705025e2b5667f90892bfaa53ed18265
keisuke <n.kei.0801.1997@gmail.com>
2022-04-02 21:07:45 +0900

    follow

--- database/migrations/2022_04_02_104449_create_follows_cast_table.php
+++ database/migrations/2022_04_02_104449_create_follows_cast_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFollowCastsTable extends Migration
+class CreateFollowsCastTable extends Migration
 {
     /**
      * Run the migrations.
