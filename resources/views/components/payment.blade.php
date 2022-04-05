39a2e69d238d01fab54ef9c416673e56471e7d04
keisuke <n.kei.0801.1997@gmail.com>
2022-04-05 20:29:56 +0900

    stripe

--- resources/views/components/payment.blade.php
+++ resources/views/components/payment.blade.php
@@ -1,6 +1,5 @@
 <script src="https://js.stripe.com/v3/"></script>
 <script src="{{asset('js/payment.js')}}"></script>
 <script>
-  var stripe_public_key = '{{config('
-  payment.stripe_secret_key')}}';
+  var stripe_public_key = "{{config('payment.stripe_secret_key')}}";
 </script>
