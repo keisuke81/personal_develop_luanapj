b252729ef2b491d9b2a807946155abd404c4988f
keisuke <n.kei.0801.1997@gmail.com>
2022-04-07 19:24:31 +0900

    stripe

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -12,4 +12,11 @@
   <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
   <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://js.stripe.com/v3/"></script>
+
+  <script>
+    var stripe_public_key = "{{config('payment.stripe_public_key')}}";
+  </script>
+
+  <script src="{{asset('js/payment.js')}}"></script>
 </head>
