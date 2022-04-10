1cea319fd46c32eb5dd2e316c29d29fbf6a8e3d9
keisuke <n.kei.0801.1997@gmail.com>
2022-04-10 17:42:15 +0900

    spa

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -21,8 +21,9 @@
   @endif
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/vegas/2.4.4/vegas.min.css">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <script src="{{asset('/js/app.js')}}"></script>
-  <script src="{{ mix('js/app.js') }}" defer></script>
+  <script src="{{asset('js/app.js')}}" defer></script>
+  <!--<script src="{{ mix('js/app.js') }}" defer></script>
+-->
   <script>
     (function(d) {
       var config = {
