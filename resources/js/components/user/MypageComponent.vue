e6c139b737d6b483ad307ac6139d4f836a9ba523
keisuke <n.kei.0801.1997@gmail.com>
2022-04-10 13:02:03 +0900

    spa

---
+++ resources/js/components/user/MypageComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <ons-page>
+    <ons-tabbar swipeable position="auto">
+    @component('components.tabbar')
+    @endcomponent
+    </ons-tabbar>
+    @component('components.menu')
+    @endcomponent
+    <div class="type_a">
+      <div class="type_a_child">
+        <div class="text-center">
+          <p>会員番号：{{$user_id}}</p>
+        </div>
+        <div>
+          <a class="btn" href="{{route('GetUserReserve')}}">今後のラウンド予定</a>
+        </div>
+
+        <div>
+          <a class="btn" href="/payment">クレジットカード情報</a>
+        </div>
+
+        <div>
+          <a class="btn" href="/mypage/receipt">ラウンド履歴</a>
+        </div>
+
+        <div>
+          <a class="btn" href="/mypage/profile/{{$user_id}}">登録情報内容を確認する</a>
+        </div>
+
+        <div>
+          <a href="/mypage/driver_license" class="btn">運転免許証の提出</a>
+        </div>
+
+        <div>
+          <a href="/logout" class="btn">ログアウト</a>
+        </div>
+      </div>
+    </div>
+  </ons-page>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
