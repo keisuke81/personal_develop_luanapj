2bd45a355b00c013ad173bac112834d0016bc3e5
keisuke <n.kei.0801.1997@gmail.com>
2022-04-10 13:58:23 +0900

    spa_mypage

--- resources/views/user/mypage.blade.php
+++ resources/views/user/mypage.blade.php
@@ -3,9 +3,9 @@
 
 <body>
   <div id="app">
-    <mypage-component v-bind:user_id="$user_id">
+    <mypage-component v-bind:user_id='{{$user_id}}'>
     </mypage-component>
-  </div>
   @component('components.footer')
   @endcomponent
+  </div>
 </body>
